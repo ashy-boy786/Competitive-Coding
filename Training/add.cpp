fe84c2e2dae7815409f39c2da670e7752864091d
ashy-boy786 <adarshkishore.201it103@nitk.edu.in>
2021-03-24 19:50:19 +0530

    Training on 24/03/2021

---
+++ Training/add.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	freopen("addin.txt", "r", stdin); // cin will 
+									  // directed to 
+									  // addin.txt
+	freopen("addout.txt", "w", stdout);
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << a + b;
+
+	return 0;
+}
